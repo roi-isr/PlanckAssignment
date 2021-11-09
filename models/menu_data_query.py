a1ef31a85a5acf8b1e8758c08eaab20a26d9b4f7
Roi Israeli <roiisra4@gmail.com>
2021-11-09 14:00:02 +0200

    add 3 flask endpiont for handling all items from a category query

--- models/menu_data_query.py
+++ models/menu_data_query.py
@@ -14,11 +14,31 @@ class MenuDataQuery:
         if not self.category_list:
             raise ValueError("Not categoriesList field in the income request's JSON")
 
-    def get_all_items_by_category(self, category: str):
+    def get_all_items_by_category(self, category: str, fields_to_filter=()):
         requested_category_list = list(
             filter(lambda cat: cat["categoryName"] == category, self.category_list)
         )
 
-        requested_dish_list = [category_item["dishList"] for category_item in requested_category_list]
+        requested_dish_list = []
+        for category_item in requested_category_list:
+            requested_dish_list += category_item["dishList"]
 
-        print(requested_dish_list)
+        return self.__extract_field_from_collection(collection=requested_dish_list, fields_to_filter=fields_to_filter)
+
+    def get_item_by_category_and_id(self, category: str, fields_to_filter=()):
+        requested_category_list = list(
+            filter(lambda cat: cat["categoryName"] == category, self.category_list)
+        )
+
+        requested_dish_list = []
+        for category_item in requested_category_list:
+            requested_dish_list += category_item["dishList"]
+
+        return self.__extract_field_from_collection(collection=requested_dish_list, fields_to_filter=fields_to_filter)
+
+    @staticmethod
+    def __extract_field_from_collection(collection, fields_to_filter):
+        if fields_to_filter:
+            return [{k: doc[k] for k in fields_to_filter} for doc in collection]
+        return collection  # in case there are no fields to filter, returns the whole collection as it is
+
--- server.py
+++ server.py
@@ -1,17 +1,34 @@
-from flask import Flask, jsonify
+"""Spinning up a Flask server that listens to incoming requests on localhost in port 5000"""
+
+from flask import Flask, jsonify, abort
 from flask_cors import CORS
+
+from models.menu_data_query import MenuDataQuery
+from utility.http_request import send_http_request
 
 app = Flask(__name__)
 CORS(app)  # enable connections from all domains
 
 
+# TODO: update daily
+@app.route('/<category>', methods=['GET'])
+def get_all_items_by_category(category):
+    if category not in ('drinks', 'pizzas', 'desserts'):
+        abort(404)
+
+    json_menu_data = send_http_request(
+        "https://www.10bis.co.il/NextApi/GetRestaurantMenu?culture=en&uiCulture=en&restaurantId=19156&deliveryMethod"
+        "=pickup"
+    )
+
+    menu_query = MenuDataQuery(json_data=json_menu_data)
+
+    selected_items_for_response = (menu_query.get_all_items_by_category(category=category.title(),
+                                                                        fields_to_filter=(
+                                                                            'dishName', 'dishId', 'dishDescription',
+                                                                            'dishPrice')))
+    return jsonify(selected_items_for_response), 200
 
 
-from utility.http_request import send_http_request
-from models.menu_data_query import MenuDataQuery
-
-json_data = send_http_request("https://www.10bis.co.il/NextApi/GetRestaurantMenu?culture=en&uiCulture=en&restaurantId=19156&deliveryMethod=pickup")
-a = MenuDataQuery(json_data=json_data)
-
-print(a.get_all_items_by_category(category="Drinks"))
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
